31869b48328b4a50c9da530a88b12d3825581580
Sparkenstein <zetabytes.pp@gmail.com>
2019-11-07 21:20:02 +0530

    working for web

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,19 @@
 <template>
   <div id="app">
-    Hello Vue
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <div>
+            <base-button type="primary">Button</base-button>
+            <base-button type="primary" icon="ni ni-bag-17">With icon</base-button>
+            <base-button type="primary" icon="ni ni-atom"></base-button>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-
-export default { }
+export default {};
 </script>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Argon from '@/argon/plugins/argon-kit';
 
+Vue.use (Argon);
 Vue.config.productionTip = false
 
 new Vue({
